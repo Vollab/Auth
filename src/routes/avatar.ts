001c5e0fd1a7ae43dfeb06c4460931de8db5393b
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2023-11-19 20:53:31 -0300

    🥅 catches `multer` `errors`

--- src/routes/avatar.ts
+++ src/routes/avatar.ts
@@ -6,6 +6,10 @@ import { avatar_model } from '../models'
 
 import { require_auth, transaction } from 'common/middlewares'
 import { BadRequestError } from 'common/errors'
+import { UploadError } from 'common/errors/upload'
+
+// https://medium.com/swlh/how-to-implement-image-upload-using-express-and-multer-postgresql-c6de64679f2
+// https://blog.rocketseat.com.br/upload-de-imagens-no-s3-da-aws-com-node-js/
 
 const MAX_SIZE = 2 * 1024 * 1024 // 2Mb
 const storage = multer.diskStorage({
@@ -20,9 +24,14 @@ const fileFilter = (req: any, file: any, cb: any) => {
 	const allowedMimes = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif']
 
 	if (allowedMimes.includes(file.mimetype)) cb(null, true)
-	else cb(new BadRequestError('Invalid file type.'))
+	else cb(new BadRequestError('Invalid file type'))
 }
-const upload = multer({ storage, limits: { fileSize: MAX_SIZE }, fileFilter })
+const upload = (req: any, res: any, next: any) => {
+	multer({ storage, limits: { fileSize: MAX_SIZE }, fileFilter }).single('avatar')(req, res, err => {
+		if (err instanceof multer.MulterError) next(new UploadError(err))
+		next(err)
+	})
+}
 const router = express.Router()
 
 router.get('/api/avatar', require_auth(['candidate', 'orderer']), async (req, res) => {
@@ -33,14 +42,14 @@ router.get('/api/avatar', require_auth(['candidate', 'orderer']), async (req, re
 	return res.status(200).setHeader('Content-Type', mime_type).sendFile(file_path)
 })
 
-router.put('/api/avatar', require_auth(['candidate', 'orderer']), upload.single('avatar'), transaction, async (req, res) => {
+router.put('/api/avatar', require_auth(['candidate', 'orderer']), upload, transaction, async (req, res) => {
 	const user_id = req.current_user!.user_id
 	const { filename: file_name, path: file_path, mimetype: mime_type, size } = req.file!
 
-	const [{ file_path: old_file_path }] = await avatar_model.delete(user_id)
+	const [old_avatar] = await avatar_model.delete(user_id)
 	const [avatar] = await avatar_model.insert({ id: user_id, file_name, file_path, mime_type, size })
 
-	if (file_name != 'default_avatar') fs.unlinkSync(old_file_path)
+	if (old_avatar.file_name != 'default_avatar') fs.unlinkSync(old_avatar.file_path)
 
 	res.status(200).json({ avatar })
 })
