3164c6c2e79e481d16fbbd4e6de1f738cebb77ad
InSTinToS <miguelandradebarreto2@gmail.com>
2023-11-22 22:36:32 -0300

    :label: updates link body types

--- src/routes/link/create.ts
+++ src/routes/link/create.ts
@@ -1,26 +1,27 @@
-import { body } from 'express-validator'
-import express from 'express'
+import { body } from "express-validator";
+import express from "express";
 
-import { link_model } from '../../models'
+import { link_model } from "../../models";
 
-import { require_auth, validate_request } from 'common/middlewares'
+import { require_auth, validate_request } from "common/middlewares";
 
-const router = express.Router()
+const router = express.Router();
+import { Link } from "types-vollab/dist/shared/link";
 
 router.post(
-	'/api/users/links',
-	require_auth(['candidate', 'orderer']),
-	body('url', 'url must not be empty').notEmpty(),
-	body('text', 'text must not be empty').notEmpty(),
-	validate_request,
-	async (req, res) => {
-		const user_id = req.current_user!.user_id
-		const { url, text } = req.body
+  "/api/users/links",
+  require_auth(["candidate", "orderer"]),
+  body("href", "href must not be empty").notEmpty(),
+  body("label", "label must not be empty").notEmpty(),
+  validate_request,
+  async (req, res) => {
+    const user_id = req.current_user!.user_id;
+    const { href, label }: Link = req.body;
 
-		const [link] = await link_model.insert({ user_id, url, text })
+    const [link] = await link_model.insert({ user_id, url: href, text: label });
 
-		res.status(201).json({ link })
-	}
-)
+    res.status(201).json({ link });
+  }
+);
 
-export { router as link_create_router }
+export { router as link_create_router };
